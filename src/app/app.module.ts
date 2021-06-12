d5a31ae8d9c9124e6241b81b49d86b93534ac339
ZhukovAlV <Olenka2012$github>
2021-06-12 22:39:20 +0600

    Add component user-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyComponent } from './my/my.component';
+import { UserListComponent } from './user/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent, MyComponent
+    AppComponent, MyComponent, UserListComponent
   ],
   imports: [
     BrowserModule,
