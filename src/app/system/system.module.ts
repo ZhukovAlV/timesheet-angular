c7dce73ac6ca78ca912aab8ddbc8891139bb0117
ZhukovAlV <Olenka2012$github>
2021-06-14 17:49:53 +0600

    add header

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from "@angular/core";
 import { SystemRoutingModule } from "./system-routing.module";
 import { SystemComponent } from "./system.component";
 import { UsersPageComponent } from './users-page/users-page.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
     imports: [
@@ -11,7 +13,7 @@ import { UsersPageComponent } from './users-page/users-page.component';
     ],
     declarations: [
       UsersPageComponent, 
-      SystemComponent
+      SystemComponent, SidebarComponent, HeaderComponent
     ]
 })
 export class SystemModule {}
