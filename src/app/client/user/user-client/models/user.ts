c2564b1192b23e012957f5460595a5ef698b8ce6
ZhukovAlV <Olenka2012$github>
2021-06-24 18:25:49 +0600

    update user

--- src/app/client/user/user-client/models/user.ts
+++ src/app/client/user/user-client/models/user.ts
@@ -1,7 +1,12 @@
 /* tslint:disable */
 export interface User {
+  accessLevel?: string;
+  firstName?: string;
+  lastName?: string;
   login?: string;
-  name?: string;
-  surname?: string;
+  middleName?: string;
+  personalNumber?: number;
+  positionEng?: string;
+  positionRus?: string;
   userId?: number;
 }
