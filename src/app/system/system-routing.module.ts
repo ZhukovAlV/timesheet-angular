6695efb4d91b70cea148c0eb818d7ce031cdff8f
ZhukovAlV <Olenka2012$github>
2021-06-24 14:58:54 +0600

    Update table for model

--- src/app/system/system-routing.module.ts
+++ src/app/system/system-routing.module.ts
@@ -4,10 +4,13 @@ import { RouterModule, Routes } from "@angular/router";
 import { SystemComponent } from "./system.component";
 import { UserListComponent } from "../user/user-list/user-list.component";
 import { UsersPageComponent } from "./users-page/users-page.component";
+import { TimedataListComponent } from "../timedata/timedata-list/timedata-list.component";
 
 const routes: Routes = [
     {path: 'system', component: SystemComponent, children: [
         {path: 'user', component: UserListComponent},
+        {path: 'timedata/:userId', component: TimedataListComponent},
+        {path: 'timedata', component: TimedataListComponent},
         {path: 'users', component: UsersPageComponent}
     ]}
 ];
--- src/app/timedata/timedata-list/timedata-list.component.ts
+++ src/app/timedata/timedata-list/timedata-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IterableTimeData } from 'src/app/client/timedata/timedata-client/models';
 import { TimeDataControllerService } from 'src/app/client/timedata/timedata-client/services';
 
@@ -9,10 +10,14 @@ import { TimeDataControllerService } from 'src/app/client/timedata/timedata-clie
 })
 export class TimedataListComponent implements OnInit {
   timeDatas!: IterableTimeData;
+  userId!: number;
 
-  constructor(private timeDataService: TimeDataControllerService) { }
+  constructor(private timeDataService:TimeDataControllerService, private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(params => {
+      this.userId = Number(params.get('userId'));
+    })
     this.timeDataService.findAllUsingGET().subscribe({
       next: timeDatas => this.timeDatas = timeDatas
     });
